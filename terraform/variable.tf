05a1470840bc49136449d96abb13d1dc46d63107
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-03 19:11:12 +0530

    files added terraform

---
+++ terraform/variable.tf
@@ -0,0 +1,20 @@
+variable "region" {
+    description = "used to specify the region"
+    default = "us-east-1"
+}
+
+variable "availability_zone" {
+    default = "us-east-1a"
+}
+
+variable "eks_cluster_role" {
+    default = "arn:aws:iam::036965198866:role/AmazonEKSClusterRole"
+}
+
+variable "eks_node_role" {
+    default = ""
+}
+
+variable "eks_encryption_key" {
+    default = ""
+}
